aa979455be5c2c8ddb9191cd9adf1f3a80d917a3
Jimbei <contrerasr2272@gmail.com>
2022-09-22 16:05:09 -0400

    finished making comment model

---
+++ controllers/comment-controller.js
@@ -0,0 +1,51 @@
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+  // add comment to pizza
+  //When you add data into a nested array of a MongoDB document, they become what's known as a "nested document" or "subdocument".
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+    .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // remove comment
+  //We then take that data and use it to identify and remove it from the associated pizza using the Mongo $pull operation.
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then(deletedComment => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: 'No comment with this id!' });
+        }
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  }
+};
+
+module.exports = commentController;
--- controllers/pizza-contollers.js
+++ controllers/pizza-contollers.js
@@ -7,6 +7,12 @@ const pizzaController = {
     //GET /api/pizzas route. It uses the Mongoose .find() method, much like the Sequelize .findAll() method.
     getAllPizza(req, res) {
         Pizza.find({})
+        .populate({
+          path: 'comments',
+          select: '-__v'
+        })
+          .select('-__v')
+          .sort({_id: -1})
           .then(dbPizzaData => res.json(dbPizzaData))
           .catch(err => {
             console.log(err);
@@ -17,6 +23,11 @@ const pizzaController = {
       // get one pizza by id
       getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+          .populate({
+          path: 'comments',
+          select: '-__v'
+        })
+         .select('-__v')
           .then(dbPizzaData => {
             // If no pizza is found, send 404
             if (!dbPizzaData) {
